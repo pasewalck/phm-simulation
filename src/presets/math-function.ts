e6b7066f11f222d623cd544987f60954a32906c1
pasewalck <j.pasewalck@posteo.net>
2026-06-25 18:58:59 +0200

    feat: improve function naming and add normal sin and fix incorrect latex

--- src/presets/math-function.ts
+++ src/presets/math-function.ts
@@ -9,32 +9,37 @@ export interface FunctionPreset {
 
 export const functionPresets: FunctionPreset[] = [
     {
-        name: 'Sägezahn (4x)',
+        name: 'Sägezahn (4x) Versetzt',
         formel: 'f(t) = 1 + \\frac{|(4t) \\bmod 2\\pi|}{2\\pi} \\cdot 0.5',
         funktion: (t) => 1 + Math.abs((t * 4) % (Math.PI * 2)) / (Math.PI * 2) * 0.5
     },
     {
-        name: 'Sägezahn (2x)',
+        name: 'Sägezahn (2x) Versetzt',
         formel: 'f(t) = 1 + \\frac{|(2t) \\bmod 2\\pi|}{2\\pi} \\cdot 0.5',
         funktion: (t) => 1 + Math.abs((t * 2) % (Math.PI * 2)) / (Math.PI * 2) * 0.5
     },
     {
-        name: 'Kosinus (8x)',
+        name: 'Kosinus (8x) Versetzt',
         formel: 'f(t) = \\frac{\\cos(8t)}{2} + 1',
         funktion: (t) => Math.cos(t * 8) / 2 + 1
     },
     {
         name: 'Kosinus',
-        formel: 'f(t) = \\frac{\\cos(t)}{2} + 1',
+        formel: 'f(t) = \\cos(t)',
         funktion: (t) => Math.cos(t)
     },
     {
-        name: 'Sinus (2x)',
+        name: 'Sinus',
+        formel: 'f(t) = \\sin(t)',
+        funktion: (t) => Math.sin(t)
+    },
+    {
+        name: 'Sinus (2x) Versetzt',
         formel: 'f(t) = \\frac{\\sin(2t)}{2} + 1',
         funktion: (t) => Math.sin(t * 2) / 2 + 1
     },
     {
-        name: 'Rechtecksignal (4x)',
+        name: 'Rechtecksignal (4x) Versetzt',
         formel: 'f(t) = \\begin{cases} 1 & |(4t) \\bmod 2\\pi| > \\pi \\\\ 0 & \\text{sonst} \\end{cases} + 0.5',
         funktion: (t) => (Math.abs((t * 4) % (Math.PI * 2)) > Math.PI ? 1 : 0) + 0.5
     },
