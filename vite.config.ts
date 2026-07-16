7a2c1bca51163768e4fea77a616d3cab6f9058c1
pasewalck <j.pasewalck@posteo.net>
2026-07-16 16:00:17 +0200

    ops

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: './',
   build: {
-    target: 'ES2020',
+    target: 'es2021',
+    cssMinify: 'lightningcss',
     rollupOptions: {
       input: {
         main: 'index.html',
